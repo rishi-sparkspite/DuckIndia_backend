be4a0835640f9ed5c675bcf47f94e0ec26d4b6cb
Rishi Chhabra <rishi.chhabra@sparkspite.com>
2024-08-08 23:11:27 +0530

    neww

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/banners", bannerRoutes);
 app.use("/api/cart", cartRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
